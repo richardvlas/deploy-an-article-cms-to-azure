30825ff2bdb41f4f52fb571b472cd94ddce3435d
Richard Vlas <vlasrichard@gmail.com>
2021-12-27 00:24:41 +0100

    referenced postfix from provision script

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,15 +1,6 @@
 # Azure CLI script to deploy the flask web app from a directory to Azure App Service.
 # Be sure to run this script with source ./deployment.sh 
 
-# Names that must be unique across Azure and thus use the postfix.
-
-# Many resource names must be unique across Azure. To ensure uniqueness,
-# set the POSTFIX environment variable to something like your name or
-# company name, whatever value is likely to be unique. 
-# This script by default uses a random number.
-
-export POSTFIX=$RANDOM
-
 echo "Provisioning Azure App Service and deploying the flask web app"
 
 cd ..
